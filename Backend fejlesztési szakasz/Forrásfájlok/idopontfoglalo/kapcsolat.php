33a1a16b1523863b7645dbcfda9219cd28a4ff9d
Orosz Kristóf <kristof.orosz.11@gmail.com>
2025-06-05 15:33:01 +0200

    A "kapcsolat.php" fájl javítása a megfelelő backend - adatbázis kapcsolathoz.

--- Backend fejlesztési szakasz/Forrásfájlok/idopontfoglalo/kapcsolat.php
+++ Backend fejlesztési szakasz/Forrásfájlok/idopontfoglalo/kapcsolat.php
@@ -1,19 +1,26 @@
 <?php
-$host = 'localhost';
-$dbname = 'idopontfoglalo';
-$username = 'root';
-$password = '';
+
+$dbHost = 'localhost';
+$dbName = 'idopontfoglalo';
+$dbUser = 'root';
+$dbPass = ''; 
 
 try {
-   
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-   
-
+    $pdo = new PDO(
+        "mysql:host=$dbHost;dbname=$dbName;charset=utf8",
+        $dbUser,
+        $dbPass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
 } catch (PDOException $e) {
     
-    die("Adatbázis kapcsolódási hiba: " . $e->getMessage());
+    header('Content-Type: application/json');
+    http_response_code(500);
+    echo json_encode([
+        'error' => 'Adatbázis kapcsolódási hiba: ' . $e->getMessage()
+    ]);
+    exit;
 }
-?>
